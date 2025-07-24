fd74ac779f88768e649616d661b323f26084db79
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-07-24 23:03:05 +0300

    add and use props for components, simple code refactoring

--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -2,12 +2,18 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({ data }) => {
   return (
     <ul className="app-list list-group">
-      <EmployeesListItem />
-      <EmployeesListItem />
-      <EmployeesListItem />
+      {data.map((employee) => (
+        <EmployeesListItem
+          key={employee.id}
+          name={employee.name}
+          salary={employee.salary}
+          increase={employee.increase}
+        />
+        // <EmployeesListItem {...employee} />
+      ))}
     </ul>
   );
 };
